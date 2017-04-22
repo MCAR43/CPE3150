888d34df9dc43c46bed072e5b83ac89546762a20
trwt33 <trwt33@mst.edu>
2017-04-22 16:30:29 -0500

    Modified main.c, program can wait on inputs from the player using the wait function. Added getMole function to return the current status of the buttons.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <reg51.h>
+#include <stdlib.h>
 sbit mole0_0 = P2^4;//LEDs
 sbit mole0_1 = P0^5;
 sbit mole0_2 = P2^7;
@@ -18,6 +19,11 @@ sbit hole1_2 = P0^0;
 sbit hole2_0 = P2^1;
 sbit hole2_1 = P0^3;
 sbit hole2_2 = P2^2;
+
+/*int swapBytes(int x)
+{
+    return ((x & 0x00FF)<< 8 | (x & 0xFF00)>>8 );
+}*/
 
 void setLED(unsigned char row, unsigned char col)
 {
@@ -129,7 +135,84 @@ void clearLED(unsigned char row, unsigned char col)
 	}	//end case switch
 }
 
+unsigned char getHole(unsigned char row, unsigned col)//Gets the value for the button
+{
+	switch(row)
+	{
+		case 0:
+		{
+			switch(col)
+			{
+				case 0:
+					return((unsigned char)hole0_0);
+					break;
+				case 1:
+					return((unsigned char)hole0_1);
+					break;
+				case 2:
+					return((unsigned char)hole0_2);
+					break;
+			}//end switch col
+		}//end case 0
+			break;
+		case 1:
+		{
+			switch(col)
+			{
+				case 0:
+					return((unsigned char)hole1_0);
+					break;
+				case 1:
+					return((unsigned char)hole1_1);
+					break;
+				case 2:
+					return((unsigned char)hole1_2);
+					break;
+			}//end switch col
+		}//end case 1
+			break;
+		case 2:
+		{
+			switch(col)
+			{
+				case 0:
+					return((unsigned char)hole2_0);
+					break;
+				case 1:
+					return((unsigned char)hole2_1);
+					break;
+				case 2:
+					return((unsigned char)hole2_2);
+					break;
+			}//end switch col
+		}//end case 2
+			break;
+	}	//end case switch
+}
 
+unsigned char wait(int difficulty, unsigned char row, unsigned char col)//Boolean is not a default type in C. Will return 1 if button is pressed on time
+{
+	
+	unsigned int index;
+	TMOD = 0x01;
+	TH0 = 0;
+	TL0 = 0;//These delays must be observable by humans, so difficulty will be number of loops
+	TR0 = 1;
+	
+	for(index = 0; index < difficulty; index++)
+	{
+		while(!TF0)//wait for the max time
+		{
+			if(getHole(row, col)	== 0)//buttons are low if pressed, getHole will generate some overhead
+			{
+				TR0 = 0;//Stop timer
+				TF0 = 0;//Reset overflow
+				return 1;//Return 1 is presses
+			}//end if
+		}//end while
+	}//end for, difficulty 
+	return 0;//button was never pressed
+}//end wait function
 
 void main(void)
 {
@@ -139,6 +222,28 @@ void main(void)
 	//P1M2 = 0;
 	//P2M0 = 0;
 	//P2M1 = 0;
+	
+	unsigned int difficulty = 860;//using the max demay for a timer, this is the number of loops that we will wait for at max time
+	
+	//Example
+	unsigned char test = wait(difficulty, 1, 1); //This will wait for about a second before returning, if pressed the function will return 1 right as it's pressed
+	//if not pressed it will wait the full timer
+	
+	while(1);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }//End main
